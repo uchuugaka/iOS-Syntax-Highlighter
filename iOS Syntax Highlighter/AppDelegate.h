c0f20c4fe3b0a5b9d05b13747664b64294d25e7d
Andrew Boos <boos1993@gmail.com>
2012-04-02 23:41:56 -0500

    Initial Commit

---
+++ iOS Syntax Highlighter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iOS Syntax Highlighter
+//
+//  Created by Andrew Boos on 4/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iOS Syntax Highlighter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS Syntax Highlighter
+//
+//  Created by Andrew Boos on 4/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
